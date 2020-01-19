df0bc07619c29f95477d407eebfd688b358d44ff
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-19 17:30:43 +0300

    Inject the gitService to the body-repo component  - add basic styling

--- src/app/body-repo/body-repo.component.ts
+++ src/app/body-repo/body-repo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Repository } from '../repository';
+import { GitService } from '../apicalls/git.service';
 
 @Component({
   selector: 'app-body-repo',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyRepoComponent implements OnInit {
 
-  constructor() { }
+  repo:Repository;
+  myUserName = 'Mutugiii';
+  constructor(public httpService:GitService) { }
 
   ngOnInit() {
+    this.httpService.repoRequest(this.myUserName)
   }
 
 }
