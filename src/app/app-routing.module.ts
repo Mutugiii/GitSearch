006c36e8602e18ac785c34086e7bbd5c8e0bbbfd
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-19 18:57:41 +0300

    Add routes and redirect to the app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyUserComponent } from './body-user/body-user.component';
+import { BodyRepoComponent } from './body-repo/body-repo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'user', component: BodyUserComponent},
+  {path:'repos', component: BodyRepoComponent},
+  { path: '', redirectTo:"/user", pathMatch:"full"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
