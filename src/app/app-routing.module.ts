dd650138a5bf2329c1fd1fbfaba7cee868251768
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-20 22:27:45 +0300

    Add the routes to deal with errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BodyUserComponent } from './body-user/body-user.component';
 import { BodyRepoComponent } from './body-repo/body-repo.component';
+import { WrongRouteComponent } from './wrong-route/wrong-route.component';
 
 
 const routes: Routes = [
   {path:'user', component: BodyUserComponent},
   {path:'repos', component: BodyRepoComponent},
-  { path: '', redirectTo:"/Mutugi", pathMatch:"full"},
-
+  { path: '', redirectTo:"/user", pathMatch:"full"},
+  {path: '**', component: WrongRouteComponent}
 ];
 
 @NgModule({
