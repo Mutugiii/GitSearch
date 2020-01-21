367b5f29ab9588c1512ddeb10ece9f7d89d373df
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-21 13:06:41 +0300

    Remove environment.prod.ts file from git

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.github.com/users/Mutugiii?access_token=4b2e8357b96c9e8b35c63590ddba8d40f1f7aca1',
-  gitUrl: 'https://api.github.com/users/',
-  accessToken: '?access_token=4b2e8357b96c9e8b35c63590ddba8d40f1f7aca1',
-  repoAccess: '/repos'
-};
