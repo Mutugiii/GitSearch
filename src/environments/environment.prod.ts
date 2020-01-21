891653f31fecc2233a132ac6f33666ed76992e1f
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-21 11:33:19 +0300

    Move api key variable to prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.github.com/users/Mutugiii?access_token=4b2e8357b96c9e8b35c63590ddba8d40f1f7aca1',
+  gitUrl: 'https://api.github.com/users/',
+  accessToken: '?access_token=4b2e8357b96c9e8b35c63590ddba8d40f1f7aca1',
+  repoAccess: '/repos'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.github.com/users/Mutugiii?access_token=4b2e8357b96c9e8b35c63590ddba8d40f1f7aca1',
-  gitUrl: 'https://api.github.com/users/',
-  accessToken: '?access_token=4b2e8357b96c9e8b35c63590ddba8d40f1f7aca1',
-  repoAccess: '/repos'
 };
 
 /*
